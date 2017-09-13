9136d914a68167cd163a3884f2f4547005ac461d
sc0Vu <alk03073135@gmail.com>
2017-09-13 18:04:10 +0800

    Add bignumber karatsuba

--- bignumber/bignumber.js
+++ bignumber/bignumber.js
@@ -3,6 +3,7 @@ module.exports = exports = function (number, base) {
 
   bignumber.number = new String(number)
   bignumber.base = base
+  bignumber.max = new String(Number.MAX_SAFE_INTEGER)
 
   bignumber.add = function (number) {
     var tmp = new String(number)
@@ -41,6 +42,42 @@ module.exports = exports = function (number, base) {
       }
     }
     return ans.join('')
+  }
+
+  bignumber.mul = function (number) {
+    var tmp = new String(number)
+    var limit = Math.floor(this.max.length / 2)
+
+    if (this.number.length < limit && tmp.length < limit) {
+      return parseInt(this.number, this.base) * parseInt(tmp, this.base)
+    }
+    return this.karatsuba(this.number, tmp)
+  }
+
+  bignumber.karatsuba = function (numbera, numberb) {
+    if (typeof numbera !== 'string') {
+      numbera = numbera.toString()
+    }
+    if (typeof numberb !== 'string') {
+      numberb = numberb.toString()
+    }
+    if (numbera.length < 2 && numberb.length < 2) {
+      return numbera * numberb
+    }
+    // karatsuba algorithm
+    var maxLength = Math.max(numbera.length, numberb.length)
+    var pivot = Math.floor(maxLength / 2)
+
+    // split digit
+    var higha = numbera.substr(0, pivot)
+    var lowa = numbera.substr(pivot)
+    var highb = numberb.substr(0, pivot)
+    var lowb = numberb.substr(pivot)
+
+    var z0 = this.karatsuba(lowa, lowb)
+    var z1 = this.karatsuba((parseInt(lowa , this.base)+ parseInt(higha, this.base)), (parseInt(lowb , this.base)+ parseInt(highb, this.base)))
+    var z2 = this.karatsuba(higha, highb)
+    return (Math.pow(z2 * 10, 2 * pivot)) + (Math.pow((z1 - z2 - z0) * 10, pivot)) + z0
   }
 
   return bignumber
--- bignumber/test.js
+++ bignumber/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert')
 var Bignumber = require('./bignumber')
 
+// test add
 var a = new Bignumber(1234567, 10)
 var ans = a.add(1234567)
 
@@ -17,3 +18,10 @@ assert.equal(ans, 11234566, '1234567 + 9999999 must equal 11234566')
 var ans = a.add(99999999)
 
 assert.equal(ans, 101234566, '1234567 + 9999999 must equal 101234566')
+
+// test mul
+var ans = a.mul(1234567)
+
+assert.equal(ans, 1524155677489, '1234567 * 1234567 must equal 1524155677489')
+
+var b = new Bignumber(123456789)
