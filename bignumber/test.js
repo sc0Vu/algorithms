5f19b6318dcad2e431f0d4a747c4b9a9ca5a41a6
sc0Vu <alk03073135@gmail.com>
2017-09-28 18:27:26 +0800

    Add test.js to run test automatically

--- bignumber/test.js
+++ bignumber/test.js
@@ -1,27 +1,45 @@
-var assert = require('assert')
+var tape = require('tape');
 var Bignumber = require('./bignumber')
 
-// test add
-var a = new Bignumber(1234567, 10)
-var ans = a.add(1234567)
+tape('Test add function', function (t) {
+  var a = new Bignumber(1234567, 10)
 
-assert.equal(ans, 2469134, '1234567 + 1234567 must equal 2469134')
+  t.test('1234567 + 1234567', function (st) {
+    var ans = a.add(1234567)
 
-var ans = a.add(11234567)
+    st.equal(ans, 2469134)
+    st.end()
+  })
 
-assert.equal(ans, 12469134, '1234567 + 11234567 must equal 12469134')
+  t.test('1234567 + 11234567', function (st) {
+    var ans = a.add(11234567)
 
-var ans = a.add(9999999)
+    st.equal(ans, 12469134)
+    st.end()
+  })
 
-assert.equal(ans, 11234566, '1234567 + 9999999 must equal 11234566')
+  t.test('1234567 + 9999999', function (st) {
+    var ans = a.add(9999999)
 
-var ans = a.add(99999999)
+    st.equal(ans, 11234566)
+    st.end()
+  })
 
-assert.equal(ans, 101234566, '1234567 + 9999999 must equal 101234566')
+  t.test('1234567 + 99999999', function (st) {
+    var ans = a.add(99999999)
 
-// test mul
-var ans = a.mul(1234567)
+    st.equal(ans, 101234566)
+    st.end()
+  })
+})
 
-assert.equal(ans, 1524155677489, '1234567 * 1234567 must equal 1524155677489')
+tape('Test mul function', function (t) {
+  var a = new Bignumber(1234567, 10)
 
-var b = new Bignumber(123456789)
+  t.test('1234567 * 99999999', function (st) {
+    var ans = a.mul(1234567)
+
+    st.equal(ans, 123456789)
+    st.end()
+  })
+})
---
+++ test.js
@@ -0,0 +1,30 @@
+var fs = require('fs-extra')
+var path = require('path')
+
+fs.readdir(__dirname)
+  .then((files) => {
+    function runTest(directory) {
+      try {
+        require(path.join(__dirname, directory, 'test.js'))
+      } catch(e) {
+        console.log(`Run test ${directory} error: ${e.message}`)
+      }
+    }
+    files = files.filter((file) => {
+      return (file !== 'node_modules' && !/^\.(.*)$/.test(file.trim()))
+    })
+
+    files.forEach(async (file) => {
+      try {
+        var stats = await fs.stat(file)
+      } catch(e) {
+        console.log(`Stat ${file} error: ${e.message}`)
+      }
+      if (stats.isDirectory()) {
+        runTest(file)
+      }
+    })
+  })
+  .catch((err) => {
+    console.log(`Read directory error: ${err.message}`)
+  })
