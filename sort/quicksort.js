a9072d2dd336f22dbd27720a389a008a80e4edf8
sc0Vu <alk03073135@gmail.com>
2017-10-12 14:29:37 +0800

    Let pivot always be right index

--- sort/quicksort.js
+++ sort/quicksort.js
@@ -1,29 +1,37 @@
 var utils = require('./utils')()
 
 module.exports = exports = function (target) {
-  var partition = function (data, low, high) {
-    var pivot = Math.floor((low+high) / 2)
-    var i = low-1
+  var partition = function (data, pivot, left, right) {
+    var tmp = data[pivot]
+    var i = left-1
+    var res = []
 
-    for (var j=low; j<high; j++) {
-      if (data[j] < data[pivot]) {
+    for (var j=left; j<right; j++) {
+      if (data[j] < tmp) {
         i += 1
-        utils.swap(data[i], data[j])
+        res = utils.swap(data[i], data[j])
+        data[i] = res[0]
+        data[j] = res[1]
       }
     }
-    utils.swap(data[i+1], data[pivot])
-    return i + 1
+    i += 1
+    res = utils.swap(data[i], data[right])
+    data[i] = res[0]
+    data[right] = res[1]
+    return i
   }
 
-  var quickSort = function (data, low, high) {
-    if (low < high) {
-      pivot = partition(data, low, high)
+  var quickSort = function (data, left, right) {
+    if (left < right) {
+      // var pivot = Math.floor((left+right) / 2)
+      var pivot = right
+      var index = partition(data, pivot, left, right)
 
-      quickSort(data, low, pivot-1)
-      quickSort(data, pivot+1, high)
+      quickSort(data, left, index-1)
+      quickSort(data, index+1, right)
       return data
     }
   }
 
-  return quickSort(target, 0, target.length)
+  return quickSort(target, 0, target.length-1)
 }
--- sort/test.js
+++ sort/test.js
@@ -1,15 +1,49 @@
 var tape = require('tape')
 var fs = require('fs-extra')
 var path = require('path')
-var target = [44,66,1,34,56]
+// var target = [44,66,1,34,56]
 var sorted = [1,34,44,56,66]
+
+tape('Test utils', (t) => {
+  var utils = require('./utils')()
+
+  t.test('swap variable a b', (st) => {
+    var a = 1
+    var b = 2
+    var res = utils.swap(a, b)
+
+    a = res[0]
+    b = res[1]
+
+    st.equals(a, 2)
+    st.equals(b, 1)
+    st.end()
+  })
+
+  t.test('swap array a[0] a[1]', (st) => {
+    var a = [1, 2]
+
+    a = utils.swap(a[0], a[1])
+    st.equals(a[0], 2)
+    st.equals(a[1], 1)
+    st.end()
+  })
+})
 
 tape('Test insertion sort', (t) => {
   var insertionSort = require('./insertionsort')
 
   t.test('sort [44,66,1,34,56]', (st) => {
-    var ans = insertionSort(target)
+    var ans = insertionSort([44,66,1,34,56])
+
     st.deepEquals(ans, sorted)
+    st.end()
+  })
+
+  t.test('sort [2,1,3,1,2]', (st) => {
+    var ans = insertionSort([2, 1, 3, 1, 2])
+
+    st.deepEquals(ans, [1, 1, 2, 2, 3])
     st.end()
   })
 })
@@ -18,8 +52,16 @@ tape('Test bubble sort', (t) => {
   var bubbleSort = require('./bubblesort')
 
   t.test('sort [44,66,1,34,56]', (st) => {
-    var ans = bubbleSort(target)
+    var ans = bubbleSort([44,66,1,34,56])
+
     st.deepEquals(ans, sorted)
+    st.end()
+  })
+
+  t.test('sort [2,1,3,1,2]', (st) => {
+    var ans = bubbleSort([2, 1, 3, 1, 2])
+
+    st.deepEquals(ans, [1, 1, 2, 2, 3])
     st.end()
   })
 })
@@ -28,8 +70,16 @@ tape('Test merge sort', (t) => {
   var mergeSort = require('./mergesort')
 
   t.test('sort [44,66,1,34,56]', (st) => {
-    var ans = mergeSort(target)
+    var ans = mergeSort([44,66,1,34,56])
+
     st.deepEquals(ans, sorted)
+    st.end()
+  })
+
+  t.test('sort [2,1,3,1,2]', (st) => {
+    var ans = mergeSort([2, 1, 3, 1, 2])
+
+    st.deepEquals(ans, [1, 1, 2, 2, 3])
     st.end()
   })
 })
@@ -38,8 +88,16 @@ tape('Test quick sort', (t) => {
   var quickSort = require('./quicksort')
 
   t.test('sort [44,66,1,34,56]', (st) => {
-    var ans = quickSort(target)
+    var ans = quickSort([44,66,1,34,56])
+
     st.deepEquals(ans, sorted)
+    st.end()
+  })
+
+  t.test('sort [2,1,3,1,2]', (st) => {
+    var ans = quickSort([2, 1, 3, 1, 2])
+
+    st.deepEquals(ans, [1, 1, 2, 2, 3])
     st.end()
   })
 })
@@ -48,19 +106,22 @@ tape('Test count inversions', (t) => {
   var countInversions = require('./countinversions')
 
   t.test('count inversions [44,66,1,34,56]', (st) => {
-    var ans = countInversions(target)
-    st.equals(ans , 0)
+    var ans = countInversions([44,66,1,34,56])
+
+    st.equals(ans , 5)
     st.end()
   })
 
   t.test('count inversions [2,4,1,3,5]', (st) => {
     var ans = countInversions([2, 4, 1, 3, 5])
+
     st.equals(ans, 3)
     st.end()
   })
 
   t.test('count inversions [2,1,3,1,2]', (st) => {
     var ans = countInversions([2, 1, 3, 1, 2])
+
     st.equals(ans, 4)
     st.end()
   })
@@ -88,19 +149,22 @@ tape('Test count inversions by merge', (t) => {
   var countInversions = require('./countInversionsbymerge')
 
   t.test('count inversions [44,66,1,34,56]', (st) => {
-    var ans = countInversions(target)
-    st.equals(ans , 0)
+    var ans = countInversions([44,66,1,34,56])
+
+    st.equals(ans , 5)
     st.end()
   })
 
   t.test('count inversions [2,4,1,3,5]', (st) => {
     var ans = countInversions([2, 4, 1, 3, 5])
+
     st.equals(ans, 3)
     st.end()
   })
 
   t.test('count inversions [2,1,3,1,2]', (st) => {
     var ans = countInversions([2, 1, 3, 1, 2])
+
     st.equals(ans, 4)
     st.end()
   })
